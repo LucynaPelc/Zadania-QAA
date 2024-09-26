ac85463ba25cf71359b1ac7a32da77ad437d9d05
LucynaPelc <lucynapelc.project@gmail.com>
2024-09-26 10:16:47 +0200

    Commit1

---
+++ script.js
@@ -0,0 +1,16 @@
+
+// Zadanie 1
+const fruits =["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+console.log (fruits);
+
+// Zadanie 2
+const numbers = [12, 34, 5, 2, 18, 24, 33, 1, 16];
+for (const number of numbers) {
+    if (number > 5) {
+        console.log (number);
+    }
+}
+
+
+
+
