65434c5aff46166e9fbe32fc5b93917d43809e64
LucynaPelc <lucynapelc.project@gmail.com>
2024-10-22 09:36:41 +0200

    Moduł6 odkomentowany

--- script1Moduł6.js
+++ script1Moduł6.js
@@ -1,38 +1,38 @@
-// // Definicja This
-// // This, stosuje się, by uzyskać dostęp do obiektu bez potzeby używania nazwy zmiennej. 
-// // Poprzez this i operator kropki odwołujemy się do konkretnego "tego" obiektu.
-// // Nazwy zmiennych sa różne ale dzięki operatorowi this możemy skopiować całą metodę do kolejnych obiektów
-// // i w tych nowych obiektach this odwoła się do nazwy nowego obiektu, bez potrzeby wpisywania jej w tej skopiwanej metodzie.
+// Definicja This
+// This, stosuje się, by uzyskać dostęp do obiektu bez potzeby używania nazwy zmiennej. 
+// Poprzez this i operator kropki odwołujemy się do konkretnego "tego" obiektu.
+// Nazwy zmiennych sa różne ale dzięki operatorowi this możemy skopiować całą metodę do kolejnych obiektów
+// i w tych nowych obiektach this odwoła się do nazwy nowego obiektu, bez potrzeby wpisywania jej w tej skopiwanej metodzie.
 
 
-// //przykład 1
-//  const box = {
-//     colors: [],
-//     getColors() {
-//         return this.colors;
-//     },
-//     addColors (newColor) {
-//         this.colors.push(newColor);
-//     },
-//  }
+//przykład 1
+ const box = {
+    colors: [],
+    getColors() {
+        return this.colors;
+    },
+    addColors (newColor) {
+        this.colors.push(newColor);
+    },
+ }
 
-// box.addColors ("pink");
-// box.addColors ("blue");
-// console.log(box.getColors());
+box.addColors ("pink");
+box.addColors ("blue");
+console.log(box.getColors());
 
-// // Dzięki this odwołujemy sie do elementów umieszczonych w obiekcie, bez wpisywania  tej nazwy (pomaga gdy nie znamy nazwy).
-// // Wpisywana metoda i this w obiekcie odnosi się do obiektu, w którym została użyta. 
+// Dzięki this odwołujemy sie do elementów umieszczonych w obiekcie, bez wpisywania  tej nazwy (pomaga gdy nie znamy nazwy).
+// Wpisywana metoda i this w obiekcie odnosi się do obiektu, w którym została użyta. 
 
 
 
-// //przykład 2
-// class pies {
-//     constructor(rasa,wiek){
-//         this.rasa = rasa;
-//         this.wiek = wiek;
-//     }
+//przykład 2
+class pies {
+    constructor(rasa,wiek){
+        this.rasa = rasa;
+        this.wiek = wiek;
+    }
   
-// }
-// console.log(pies);
+}
+console.log(pies);
 
-// //This wywołuje rasę lub wiek psa
+//This wywołuje rasę lub wiek psa
--- script2Moduł6.js
+++ script2Moduł6.js
@@ -1,13 +1,13 @@
-// //Zastosowanie praktyczne:
+//Zastosowanie praktyczne:
 
-// const Maciej={
-//     userName: "Maciej",
-//     showName(){
-//         return this.userName;
-//     },
-// }
-// console.log(showName);
+const Maciej={
+    userName: "Maciej",
+    showName(){
+        return this.userName;
+    },
+}
+console.log(showName);
 
-// //Obiekt Maciej, posiada elementy username z danymi w string, 
-// //posiada metodę showName która zwraca nam za pomoca this i kropki 
-// //dane z UserName.
+//Obiekt Maciej, posiada elementy username z danymi w string, 
+//posiada metodę showName która zwraca nam za pomoca this i kropki 
+//dane z UserName.
