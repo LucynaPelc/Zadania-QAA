d943e05598afb929dbf647b771678e3eb9df66f3
LucynaPelc <lucynapelc.project@gmail.com>
2024-10-29 16:36:29 +0100

    Moduł5.ZDomowe

--- script.Moduł5.js
+++ script.Moduł5.js
@@ -1,20 +1,20 @@
 
-// // funkcja JS numer 1
+// funkcja JS numer 1
 
-// const numbers = [12, 14, 16, 18];
-// const dubbleNumbers = numbers.map (number => number *2);
-// console.log (dubbleNumbers);
+const numbers = [12, 14, 16, 18];
+const dubbleNumbers = numbers.map (number => number *2);
+console.log (dubbleNumbers);
 
 
-// //funkcja JS numer 2
+//funkcja JS numer 2
 
-// const students = [
-//     { name: "Jan", score: 83 },
-//     { name: "Anna", score: 59 },
-//     { name: "Aleks", score: 37 },
-//     { name: "Zofia", score: 94 },
-//     { name: "Marek", score: 64 },
-//   ];
+const students = [
+    { name: "Jan", score: 83 },
+    { name: "Anna", score: 59 },
+    { name: "Aleks", score: 37 },
+    { name: "Zofia", score: 94 },
+    { name: "Marek", score: 64 },
+  ];
   
-//   const names = students.map(student => student.name);
-//   console.log(names); 
+  const names = students.map(student => student.name);
+  console.log(names); 
