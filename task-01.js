9eb6d20a5b2642fb7431e68de0fa97d7db16907f
LucynaPelc <lucynapelc.project@gmail.com>
2024-11-08 11:30:29 +0100

    Moduł 7 Zadanie, pliki JS

---
+++ task-01.js
@@ -0,0 +1,14 @@
+//Zadanie 1
+const categories = document.querySelectorAll('#categories .item');
+console.log("Number of categories: " + categories.length);
+
+//Zadanie 2
+categories.forEach(category =>{
+    const categoryName = category.querySelector('h2').textContent;
+    console.log("Category: " + categoryName);
+    const categories = category.querySelectorAll ('li');
+    console.log("Elements: " + categories.length );
+})
+
+
+
---
+++ task-02.js
@@ -0,0 +1,17 @@
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const lists = document.querySelectorAll('#ingredients');
+
+ingredients.forEach((ingredient,index) =>{
+  const li = document.createElement ('li');
+  li.classList.add ('item');
+  
+})
+console.log(ingredients);
+lists.append(ingredients);
